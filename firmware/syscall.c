1fed9fedf5b400796687f9437d6ac7e76a087025
Aaron Heise <aaron.heise@gmail.com>
2016-08-05 11:06:24 -0500

    Missed a couple things

--- firmware/syscall.c
+++ firmware/syscall.c
@@ -14,7 +14,7 @@
 //BYTE __get_system_is_threaded();
 
 /*------------------------------------------------------------------------*/
-/* Create a Synchronization Object
+/* Create a Synchronization Object                                        */
 /*------------------------------------------------------------------------*/
 /* This function is called in f_mount() function to create a new
 /  synchronization object, such as semaphore and mutex. When a 0 is returned,
